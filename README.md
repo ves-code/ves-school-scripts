cccef4b6d248317bcb3682aeb77338735f43e8ee
Omar Valsson <omar.valsson@gmail.com>
2017-02-12 18:14:02 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ves-school-scripts
-Installation scripts for the MARVEL VES school in Lugano February 2017
+Installation scripts for the MARVEL VES school in Lugano February 2017. 
+
+Scripts to install on Ubuntu 14.04 the software needed for the VES tutorial
+
+
---
+++ install-ubuntu-packages.sh
@@ -0,0 +1,29 @@
+packages="
+libmatheval-dev
+libfftw3-dev
+gsl-bin
+libgsl0-dev
+libopenmpi-dev
+openmpi-bin
+graphviz
+doxygen-latex
+libatlas-base-dev
+git
+cmake
+gnuplot-qt
+grace
+build-essential
+python-gnuplot
+vim
+emacs
+gfortran
+python-numpy
+python-scipy
+python-matplotlib
+"
+
+for p in ${packages}
+do
+  echo ${p}
+  #isudo apt-get install -y ${p}
+done
