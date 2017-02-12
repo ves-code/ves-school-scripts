df9a7d3689e83b4812d98a1c0f2c8fc64849b322
Omar Valsson <omar.valsson@gmail.com>
2017-02-12 18:31:55 +0100

    vmd

--- install-vmd.sh
+++ install-vmd.sh
@@ -1,1 +1,9 @@
 wget http://www.ks.uiuc.edu/Research/vmd/vmd-1.9.3/files/final/vmd-1.9.3.bin.LINUXAMD64-CUDA8-OptiX4-OSPRay111p1.opengl.tar.gz
+tar xvf `ls vmd-1.9.3*tar.gz`
+cd vmd-1.9.3
+./configure
+cd src
+sudo make install
+cd ../../
+rm -f vmd-1.9.3*tar.gz
+
