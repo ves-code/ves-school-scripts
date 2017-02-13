82b5cc86bd5eb3655d141342d0697681914873db
Omar Valsson <omar.valsson@gmail.com>
2017-02-13 18:22:03 +0100

    rename manual

--- install-ves-tutorial.sh
+++ install-ves-tutorial.sh
@@ -41,7 +41,7 @@ rm -f gh-pages.zip
 mv doc-ves-master-gh-pages ${INSTALL_DIR}/VES-Manual
 if [[ "$MANUAL_DESKTOP_SHORTCUTS" == "YES" ]]
 then
-  ln -sf ${INSTALL_DIR}/VES-Manual/index.html ${HOME}/Desktop/VES-Manual
+  ln -sf ${INSTALL_DIR}/VES-Manual/index.html ${HOME}/Desktop/PLUMED-Manual
   ln -sf ${INSTALL_DIR}/VES-Manual/user-doc/html/ves_tutorial_lugano_2017.html ${HOME}/Desktop/VES-Tutorials
 fi
 ################################
