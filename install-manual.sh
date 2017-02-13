c90c6a9610bf403246dee5a9918990debe59598b
Omar Valsson <omar.valsson@gmail.com>
2017-02-13 07:37:05 +0100

    install manual

--- install-manual.sh
+++ install-manual.sh
@@ -1,7 +1,12 @@
+rm -rf ${HOME}/VES-Manual
+${HOME}/Desktop/VES-Manual
+${HOME}/Desktop/VES-Tutorials
+
+
 wget http://github.com/ves-code/doc-ves-master/archive/gh-pages.zip
 unzip gh-pages.zip 
 rm -f gh-pages.zip
 mv doc-ves-master-gh-pages ${HOME}/VES-Manual
-ln -sf ${HOME}/VES-Manual/index.html ${HOME}/Desktop/VES_Manual
-ln -sf ${HOME}/VES-Manual/user-doc/html/ves_tutorial_lugano_2017.html ${HOME}/Desktop/VES_Tutorials
+ln -sf ${HOME}/VES-Manual/index.html ${HOME}/Desktop/VES-Manual
+ln -sf ${HOME}/VES-Manual/user-doc/html/ves_tutorial_lugano_2017.html ${HOME}/Desktop/VES-Tutorials
 
